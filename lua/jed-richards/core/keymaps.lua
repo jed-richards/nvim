d5d72932cf8ba43182411fa27641b000756cdba3
jed-richards <jed22richards@gmail.com>
2024-01-04 19:49:41 -0600

    <ctrl-d/u>zz keymaps

--- lua/jed-richards/core/keymaps.lua
+++ lua/jed-richards/core/keymaps.lua
@@ -7,7 +7,7 @@ local map = vim.keymap.set
 -- File Explorer (NetRW)
 map("n", "<leader>pv", vim.cmd.Ex)
 
--- Neviagation
+-- Window Navigation
 map("n", "<C-h>", "<C-w>h")
 map("n", "<C-j>", "<C-w>j")
 map("n", "<C-k>", "<C-w>k")
@@ -17,3 +17,6 @@ map("n", "<C-l>", "<C-w>l")
 map("v", ">", ">gv")
 map("v", "<", "<gv")
 
+-- Text Navigation
+map("n", "<C-d>", "<C-d>zz") -- scroll down and center
+map("n", "<C-u>", "<C-u>zz") -- scroll up and center
