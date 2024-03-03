87966c89bc322d0c68cfdf7ba7a747e0f1ddfd27
jed-richards <jed22richards@gmail.com>
2024-03-03 17:32:40 -0600

    keymaps

--- lua/jed-richards/core/keymaps.lua
+++ lua/jed-richards/core/keymaps.lua
@@ -14,9 +14,15 @@ map("n", "<C-k>", "<C-w>k")
 map("n", "<C-l>", "<C-w>l")
 
 -- Text manipulation
-map("v", ">", ">gv")
-map("v", "<", "<gv")
+map("v", ">", ">gv") -- indents
+map("v", "<", "<gv") -- indents
+map("v", "J", ":m '>+1<CR>gv=gv") -- move text down
+map("v", "K", ":m '<-2<CR>gv=gv") -- move text up
 
 -- Text Navigation
 map("n", "<C-d>", "<C-d>zz") -- scroll down and center
 map("n", "<C-u>", "<C-u>zz") -- scroll up and center
+
+-- Next and Prev will now center the screen to where the word is
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
