1f454f2e171f369b6e86bf6551e3c76be260f8c9
jed-richards <jed22richards@gmail.com>
2024-07-24 17:53:04 -0500

    remove rust-analyzer inlayHints

--- lua/jed-richards/lsp/servers/rust_analyzer.lua
+++ lua/jed-richards/lsp/servers/rust_analyzer.lua
@@ -1,3 +1,6 @@
+-- For more settings see:
+-- https://github.com/rust-lang/rust-analyzer/blob/master/docs/user/generated_config.adoc
+
 return {
 	settings = {
 		["rust-analyzer"] = {
@@ -5,6 +8,12 @@ return {
 				command = "clippy",
 			},
 			-- inlayHints = {
+			-- 	typeHints = {
+			-- 		enable = true,
+			-- 	},
+			-- 	chainingHints = {
+			-- 		enable = true,
+			-- 	},
 			-- 	lifetimeElisionHints = {
 			-- 		enable = true,
 			-- 		useParameterNames = true,
