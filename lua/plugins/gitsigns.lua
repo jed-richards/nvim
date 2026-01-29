8bf9b540c30624ffd871c1c73fd74f2f6e85a432
jed-richards <jrichards@csgactuarial.com>
2026-01-29 16:12:39 -0600

    feat: add gitsigns.nvim

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,46 @@
+return {
+	-- See `:help gitsigns`
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		local gitsigns = require("gitsigns")
+
+		gitsigns.setup({
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+
+			on_attach = function(bufnr)
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+					end
+				end, { desc = "Next hunk" })
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end, { desc = "Prev hunk" })
+
+				-- TODO: look at keymaps
+				--
+				-- https://github.com/lewis6991/gitsigns.nvim?tab=readme-ov-file#-keymaps
+			end,
+		})
+	end,
+}
