c7853a64c469c3c9c3516e56d7ab9611a53cd30a
jed-richards <jrichards@csgactuarial.com>
2026-01-29 16:09:41 -0600

    feat: init basic config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,52 @@
+-- Leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Aliases
+local map = vim.keymap.set
+
+-- Window Navigation
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+
+-- -- Splits resizing (from Teej)
+-- -- <M-,> is Alt+,
+-- map("n", "<M-,>", "<C-w>5<")
+-- map("n", "<M-.>", "<C-w>5>")
+-- map("n", "<M-t>", "<C-w>+")
+-- map("n", "<M-s>", "<C-w>-")
+
+-- Text manipulation
+map("v", ">", ">gv") -- indents
+map("v", "<", "<gv") -- indents
+map("v", "J", ":m '>+1<CR>gv=gv") -- move text down
+map("v", "K", ":m '<-2<CR>gv=gv") -- move text up
+
+-- Text Navigation
+map("n", "<C-d>", "<C-d>zz") -- scroll down and center
+map("n", "<C-u>", "<C-u>zz") -- scroll up and center
+
+-- Next and Prev will now center the screen to where the word is
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- Clear highlight
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Tmux Sessionizer
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", function()
+	vim.diagnostic.jump({ count = -1 })
+end, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", function()
+	vim.diagnostic.jump({ count = 1 })
+end, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>td", function()
+	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { silent = true, noremap = true, desc = "[T]oggle [D]iagnostics" })
---
+++ lua/config/options.lua
@@ -0,0 +1,62 @@
+-- Search and replace stuff
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.smartcase = true -- smart case
+vim.opt.inccommand = "split" -- Preview substitutions live, as you type!
+
+-- Tab and Indent
+vim.opt.tabstop = 4 -- tabs are 4 spaces
+vim.opt.softtabstop = 4 -- tabs are 4 spaces
+vim.opt.shiftwidth = 4 -- number of spaces to use on indent
+vim.opt.expandtab = true -- turns tabs to spaces
+vim.opt.smartindent = true -- This sets smartindent
+
+-- Backup and Swap Files
+vim.opt.backup = false -- creates a backup file
+vim.opt.swapfile = false -- creates a swapfile
+vim.opt.undofile = true -- enable persistent undo
+vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+
+-- Looks and Scrolling
+vim.opt.cursorline = true -- highlight the current line
+vim.opt.number = true -- set numbered lines
+vim.opt.relativenumber = true -- set relative numbered lines
+vim.opt.colorcolumn = "80" -- column at 80 to keep files "clean"
+vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.signcolumn = "yes" -- signcolumn to the left of numbers
+vim.opt.guicursor = "" -- This makes the cursor fat (Me likey...)
+vim.opt.wrap = false -- display lines as one long line
+vim.opt.showmode = false -- to see things like -- INSERT --
+
+-- allows h and l to move to next line if at last character
+vim.cmd([[set whichwrap+=h,l]])
+
+-- allows word-with-dashes to be seen as one word
+vim.cmd([[set iskeyword+=-]])
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Mouse and Clipboard
+vim.cmd([[ set clipboard+=unnamedplus ]])
+
+-- Random
+vim.opt.fileencoding = "utf-8" -- the encoding written to a file
+vim.o.termguicolors = true -- set term gui colors (most terminals support this)
+
+-- Decrease update time
+vim.opt.timeoutlen = 300
+vim.opt.updatetime = 250
+
+-- Sets how neovim will display certain whitespace in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- -- Obsidian.nvim
+-- vim.opt.conceallevel = 2
+vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
+
+vim.g.have_nerd_font = true
