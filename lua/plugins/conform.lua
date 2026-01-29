d6f2692f849c2d193d3b6ed34dc6763eae37d42d
jed-richards <jrichards@csgactuarial.com>
2026-01-29 16:14:15 -0600

    feat: add conform.nvim

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,35 @@
+return {
+	"stevearc/conform.nvim",
+
+	---@type conform.setupOpts
+	opts = {
+		notify_on_error = false,
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+		formatters_by_ft = {
+			lua = { "stylua" },
+
+			-- For standard JavaScript and React files (.js, .jsx)
+			javascript = { "prettierd", "prettier" },
+			javascriptreact = { "prettierd", "prettier" },
+
+			-- For TypeScript and React files (.ts, .tsx)
+			typescript = { "prettierd", "prettier" },
+			typescriptreact = { "prettierd", "prettier" },
+
+			python = {
+				-- To fix auto-fixable lint errors.
+				-- "ruff_fix",
+				-- To run the Ruff formatter.
+				"ruff_format",
+				-- To organize the imports.
+				"ruff_organize_imports",
+			},
+			json = { "prettier" },
+			jsonc = { "prettier" },
+			markdown = { "prettier" },
+		},
+	},
+}
