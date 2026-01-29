c1dadb478fdf47142d2685e4f1744a4016b0ec39
jed-richards <jrichards@csgactuarial.com>
2026-01-29 16:19:50 -0600

    chore: format with stylua

--- lsp/jsonls.lua
+++ lsp/jsonls.lua
@@ -1,16 +1,16 @@
 ---@type vim.lsp.Config
 return {
-	cmd = { "vscode-json-language-server", "--stdio" },
-	filetypes = { "json", "jsonc" },
-	root_markers = { ".git", "package.json" },
-	init_options = {
-		-- disable formatting for JSON; we'll use prettier
-		provideFormatter = false,
-	},
-	settings = {
-		json = {
-			schemas = require("schemastore").json.schemas(),
-			validate = { enable = true },
-		},
-	},
+  cmd = { "vscode-json-language-server", "--stdio" },
+  filetypes = { "json", "jsonc" },
+  root_markers = { ".git", "package.json" },
+  init_options = {
+    -- disable formatting for JSON; we'll use prettier
+    provideFormatter = false,
+  },
+  settings = {
+    json = {
+      schemas = require("schemastore").json.schemas(),
+      validate = { enable = true },
+    },
+  },
 }
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,92 +2,100 @@ vim.lsp.set_log_level("WARN")
 
 -- List of servers to enable
 local servers = {
-	"jsonls",
-	"lua_ls",
-	"pyright",
-	"ruff",
-	"ts_ls",
+  "jsonls",
+  "lua_ls",
+  "pyright",
+  "ruff",
+  "ts_ls",
 }
 
 local M = {}
 
 -- Simple setup function
 function M.setup()
-	-- Basic diagnostic configuration
-	vim.diagnostic.config({
-		virtual_lines = {
-			current_line = true,
-		},
-	})
+  -- Basic diagnostic configuration
+  vim.diagnostic.config({
+    virtual_lines = {
+      current_line = true,
+    },
+  })
 
-	-- Add blink.cmp capabilities to all servers
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
-	local ok, blink = pcall(require, "blink.cmp")
-	if ok then
-		capabilities = vim.tbl_deep_extend("force", capabilities, blink.get_lsp_capabilities())
-	end
+  -- Add blink.cmp capabilities to all servers
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  local ok, blink = pcall(require, "blink.cmp")
+  if ok then
+    capabilities =
+      vim.tbl_deep_extend("force", capabilities, blink.get_lsp_capabilities())
+  end
 
-	-- Set capabilities for all servers that don't specify their own
-	vim.lsp.config("*", { capabilities = capabilities })
+  -- Set capabilities for all servers that don't specify their own
+  vim.lsp.config("*", { capabilities = capabilities })
 
-	-- Setup additional keymaps that aren't in defaults
-	vim.api.nvim_create_autocmd("LspAttach", {
-		callback = function(args)
-			local bufnr = args.buf
-			local client = vim.lsp.get_client_by_id(args.data.client_id)
-			local builtin = require("telescope.builtin")
+  -- Setup additional keymaps that aren't in defaults
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local bufnr = args.buf
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      local builtin = require("telescope.builtin")
 
-			-- Set some keymaps. Using some defaults just with telescope.
-			-- - gd  (go to definition)
-			-- - grr (references)
-			-- - grt (type definition)
-			-- - gri (go to implementation)
-			-- - gO  (symbols)
-			--
-			-- Default
-			-- - grn (rename)
-			-- - gra (code action)
+      -- Set some keymaps. Using some defaults just with telescope.
+      -- - gd  (go to definition)
+      -- - grr (references)
+      -- - grt (type definition)
+      -- - gri (go to implementation)
+      -- - gO  (symbols)
+      --
+      -- Default
+      -- - grn (rename)
+      -- - gra (code action)
 
-			vim.keymap.set("n", "gd", function()
-				vim.lsp.buf.definition()
-			end, { buffer = bufnr, desc = "LSP: Go to definition" })
+      vim.keymap.set("n", "gd", function()
+        vim.lsp.buf.definition()
+      end, { buffer = bufnr, desc = "LSP: Go to definition" })
 
-			vim.keymap.set("n", "grr", function()
-				builtin.lsp_references()
-			end, { buffer = bufnr, desc = "LSP: Get references" })
+      vim.keymap.set("n", "grr", function()
+        builtin.lsp_references()
+      end, { buffer = bufnr, desc = "LSP: Get references" })
 
-			vim.keymap.set("n", "grt", function()
-				builtin.lsp_type_definitions()
-			end, { buffer = bufnr, desc = "LSP: Go to type definition" })
+      vim.keymap.set("n", "grt", function()
+        builtin.lsp_type_definitions()
+      end, { buffer = bufnr, desc = "LSP: Go to type definition" })
 
-			if client and client:supports_method("textDocument/implementation") then
-				vim.keymap.set("n", "gri", function()
-					builtin.lsp_implementations()
-				end, { buffer = bufnr, desc = "LSP: Go to implementation" })
-			end
+      if client and client:supports_method("textDocument/implementation") then
+        vim.keymap.set("n", "gri", function()
+          builtin.lsp_implementations()
+        end, { buffer = bufnr, desc = "LSP: Go to implementation" })
+      end
 
-			vim.keymap.set("n", "gO", function()
-				builtin.lsp_document_symbols()
-			end, { buffer = bufnr, desc = "LSP: Get document symbols" })
+      vim.keymap.set("n", "gO", function()
+        builtin.lsp_document_symbols()
+      end, { buffer = bufnr, desc = "LSP: Get document symbols" })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP: Hover Documentation" })
+      vim.keymap.set(
+        "n",
+        "K",
+        vim.lsp.buf.hover,
+        { buffer = bufnr, desc = "LSP: Hover Documentation" }
+      )
 
-			-- Document highlighting
-			if client and client:supports_method("textDocument/documentHighlight") then
-				vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-					buffer = bufnr,
-					callback = vim.lsp.buf.document_highlight,
-				})
-				vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-					buffer = bufnr,
-					callback = vim.lsp.buf.clear_references,
-				})
-			end
-		end,
-	})
+      -- Document highlighting
+      if
+        client and client:supports_method("textDocument/documentHighlight")
+      then
+        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+          buffer = bufnr,
+          callback = vim.lsp.buf.document_highlight,
+        })
+        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+          buffer = bufnr,
+          callback = vim.lsp.buf.clear_references,
+        })
+      end
+    end,
+  })
 
-	-- Enable all servers (configs loaded automatically from lsp/ directory)
-	vim.lsp.enable(servers)
+  -- Enable all servers (configs loaded automatically from lsp/ directory)
+  vim.lsp.enable(servers)
 end
 
 return M
