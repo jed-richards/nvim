4299317b69190fefb5d65c1222aa9988dad90e98
jed-richards <jrichards@csgactuarial.com>
2026-01-29 16:15:54 -0600

    feat: add pyright and ruff lsp config

---
+++ lsp/ruff.lua
@@ -0,0 +1,21 @@
+-- Ruff language server configuration
+
+---@type vim.lsp.Config
+return {
+	cmd = { "ruff", "server", "--preview" },
+	filetypes = { "python" },
+	root_markers = {
+		"pyproject.toml",
+		"ruff.toml",
+		".ruff.toml",
+		"setup.py",
+		"setup.cfg",
+		"requirements.txt",
+		"Pipfile",
+		".git",
+	},
+	on_attach = function(client)
+		-- disable hover in favor of Pyright
+		client.server_capabilities.hoverProvider = false
+	end,
+}
