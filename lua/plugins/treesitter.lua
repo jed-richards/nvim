68175d955e07ecc53c2950711b7bae2f8c2307e4
jed-richards <jrichards@csgactuarial.com>
2026-01-29 16:12:17 -0600

    feat: add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+
+	config = function()
+		local parsers = {
+			"python",
+			"typescript",
+			"tsx",
+		}
+
+		-- TODO: install parsers like so
+		require("nvim-treesitter").install(parsers)
+
+		-- Create autocommand to enable treesitter for installed parsers
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = parsers,
+			callback = function()
+				-- syntax highlighting, provided by Neovim
+				vim.treesitter.start()
+				-- folds, provided by Neovim
+				-- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				-- vim.wo.foldmethod = "expr"
+				-- indentation, provided by nvim-treesitter
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+	end,
+}
