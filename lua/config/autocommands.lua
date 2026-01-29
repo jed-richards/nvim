db709aa1ec484796bc0320aea95146bf99236fa9
jed-richards <jrichards@csgactuarial.com>
2026-01-29 16:10:22 -0600

    feat: add autocommands

---
+++ lua/config/autocommands.lua
@@ -0,0 +1,25 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Highlight when yanking
+autocmd("TextYankPost", {
+	desc = "Highlight when yanking text",
+	group = augroup("codemonkey-highlight-yank", {
+		clear = true,
+	}),
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
+
+-- Remove trailing whitespace on write
+autocmd({ "BufWritePre" }, {
+	group = augroup("codemonkey-remove-trailing-ws", {
+		clear = true,
+	}),
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
+})
