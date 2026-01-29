4c89c5dabb02c09c362fed38aa155d9ae881dcff
jed-richards <jrichards@csgactuarial.com>
2026-01-29 16:11:23 -0600

    feat: add colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,27 @@
+return {
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("rose-pine").setup({
+				variant = "auto", -- auto, main, moon, or dawn
+				dark_variant = "main", -- main, moon, or dawn
+				dim_inactive_windows = true,
+				extend_background_behind_borders = true,
+
+				styles = {
+					bold = true,
+					italic = true,
+					transparency = false,
+				},
+			})
+
+			vim.cmd("colorscheme rose-pine")
+			-- vim.cmd("colorscheme rose-pine-main")
+			-- vim.cmd("colorscheme rose-pine-moon")
+			-- vim.cmd("colorscheme rose-pine-dawn")
+		end,
+	},
+}
