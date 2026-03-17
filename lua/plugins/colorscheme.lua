3ed452505765b6aa6aaf7c90b36914fc85610d66
jed-richards <jrichards@csgactuarial.com>
2026-03-17 08:12:58 -0500

    feat(color): add cyberdream

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,33 +1,39 @@
 return {
+  -- colorschemes
   {
     -- https://github.com/dchinmay2/alabaster.nvim
     "dchinmay2/alabaster.nvim",
+    enabled = false,
     lazy = false,
     priority = 1000,
+    dependencies = {
+      "f-person/auto-dark-mode.nvim",
+    },
     config = function()
       vim.cmd("colorscheme alabaster")
+
+      -- Setup auto dark mode
+      require("auto-dark-mode").setup({
+        update_interval = 3000,
+        set_dark_mode = function()
+          vim.o.background = "dark"
+        end,
+        set_light_mode = function()
+          vim.o.background = "light"
+        end,
+      })
     end,
   },
   {
-    "f-person/auto-dark-mode.nvim",
-    opts = {
-      update_interval = 3000,
-      set_dark_mode = function()
-        vim.o.background = "dark"
-      end,
-      set_light_mode = function()
-        vim.o.background = "light"
-      end,
-    },
-  },
-  {
     "kepano/flexoki-neovim",
+    enabled = false,
     name = "flexoki",
     lazy = true,
   },
   {
     "rose-pine/neovim",
     name = "rose-pine",
+    enabled = false,
     lazy = true,
     config = function()
       require("rose-pine").setup({
@@ -40,6 +46,34 @@ return {
           italic = true,
           transparency = false,
         },
+      })
+    end,
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    dependencies = {
+      "f-person/auto-dark-mode.nvim",
+    },
+    config = function()
+      require("cyberdream").setup({
+        variant = "auto",
+        saturation = 1,
+        hide_fillchars = false,
+      })
+
+      vim.cmd.colorscheme("cyberdream")
+
+      -- Setup auto dark mode
+      require("auto-dark-mode").setup({
+        update_interval = 3000,
+        set_dark_mode = function()
+          vim.o.background = "dark"
+        end,
+        set_light_mode = function()
+          vim.o.background = "light"
+        end,
       })
     end,
   },
